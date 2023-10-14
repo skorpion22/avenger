97623f983d97ead51fddf53a1b483f26146548c8
unknown <srochaz@icloud.com>
2023-10-13 22:26:38 -0600

    partfinish

--- src/components/Salvador.jsx
+++ src/components/Salvador.jsx
@@ -1,6 +1,19 @@
 const Salvador = () => {
   return (
-    <div>Salvador</div>
+    <article className="py-10">
+      <header>
+        <img className="w-[200px] mx-auto " src="/zerg.jpg" alt="" />
+        <h3 className="font-bold text-[30px]">Salvador Rocha</h3>
+        <div className="w-[400px] mx-auto p-4">
+          <p>
+            Lic. Mercadotecnia y Publicidad, actualmente me dedico a la reparación
+            de equipos de computo y celulares, tambien trabajo como conductor en
+            plataforma, y estoy estudiando en Academlo para reemplazar el trabajo
+            de conductor, por algo mas fijo sin manejar tanto
+          </p>
+        </div>
+      </header>
+    </article>
   )
 }
 
